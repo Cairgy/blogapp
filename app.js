6cf2fed6f2fb7b375dfea52a3de6085b0dd652b2
Cairgy <kgmmapetla2@gmail.com>
2023-02-20 18:31:44 +0200

    before database

--- app.js
+++ app.js
@@ -11,5 +11,16 @@ app.listen(3000);
 
 app.get('/',(req,res) => {
     res.render('index');
+});
 
+app.get('/about',(req,res) => {
+    res.render('about');
+});
+
+app.get('/blogs/create',(req,res) => {
+    res.render('create');
+});
+
+app.use((req,res) => {
+    res.status(404).render('404');
 });
