1fb19e70192b1c3fc8f9e411187e446d6d82f318
Cairgy <kgmmapetla2@gmail.com>
2023-02-21 02:42:29 +0200

    middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,18 +10,27 @@ app.set('view engine', 'ejs');
 // listen for requests
 app.listen(3000);
 
-app.get('/',(req,res) => {
-    res.render('index');
+// middleware static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'my first post', snippet: 'and the 1st snippet'},
+        {title: 'my 2nd post', snippet: 'and the 2nd snippet'},
+        {title: 'my 3rd post', snippet: 'and the 3rd snippet'},
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
-app.get('/about',(req,res) => {
-    res.render('about');
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create',(req,res) => {
-    res.render('create');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
 });
 
-app.use((req,res) => {
-    res.status(404).render('404');
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
 });
